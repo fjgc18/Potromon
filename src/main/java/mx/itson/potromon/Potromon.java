d6e578b1f22ae095aebab1adaeb7982b818fdbdc
Thinkpad <Thinkpad@192.168.1.69>
2024-11-22 20:36:07 -0700

    OK

---
+++ src/main/java/mx/itson/potromon/Potromon.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package mx.itson.potromon;
+
+/**
+ *
+ * @author Thinkpad
+ */
+public class Potromon {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
