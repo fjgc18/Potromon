d02af6a6b58a9c41fb0c8f958484b84a8e6ebf22
emili <emili@Emiliano>
2024-11-27 10:26:37 -0700

    Creacion de la Clase Entidad Entrenador

---
+++ src/main/java/mx/itson/potromon/entidades/Entrenador.java
@@ -0,0 +1,74 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mx.itson.potromon.entidades;
+
+/**
+ *
+ * @author emili
+ */
+public class Entrenador {
+
+    /**
+     * @return the idEntrenador
+     */
+    public int getIdEntrenador() {
+        return idEntrenador;
+    }
+
+    /**
+     * @param idEntrenador the idEntrenador to set
+     */
+    public void setIdEntrenador(int idEntrenador) {
+        this.idEntrenador = idEntrenador;
+    }
+
+    /**
+     * @return the nombreEntrenador
+     */
+    public String getNombreEntrenador() {
+        return nombreEntrenador;
+    }
+
+    /**
+     * @param nombreEntrenador the nombreEntrenador to set
+     */
+    public void setNombreEntrenador(String nombreEntrenador) {
+        this.nombreEntrenador = nombreEntrenador;
+    }
+
+    /**
+     * @return the aliasEntrenador
+     */
+    public String getAliasEntrenador() {
+        return aliasEntrenador;
+    }
+
+    /**
+     * @param aliasEntrenador the aliasEntrenador to set
+     */
+    public void setAliasEntrenador(String aliasEntrenador) {
+        this.aliasEntrenador = aliasEntrenador;
+    }
+
+    /**
+     * @return the puebloOrigen
+     */
+    public String getPuebloOrigen() {
+        return puebloOrigen;
+    }
+
+    /**
+     * @param puebloOrigen the puebloOrigen to set
+     */
+    public void setPuebloOrigen(String puebloOrigen) {
+        this.puebloOrigen = puebloOrigen;
+    }
+    private int idEntrenador;
+    private String nombreEntrenador;
+    private String aliasEntrenador;
+    private String puebloOrigen;
+    
+    
+}
