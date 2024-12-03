77f68e9150893585744d353e7470501bd4b8e2b9
Julian716 <alex.313.rodriguez@gmail.com>
2024-12-03 16:50:20 -0700

    Adjuntar imagenes a potromones
    Modifique e boton de show potrocard para que mueste una imagen cuando selecciones un potromon

---
+++ Potromon-master/src/main/java/mx/itson/potromon/ui/Potromon.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package mx.itson.potromon.ui;
+import mx.itson.potromon.entidades.Entrenador;
+import mx.itson.potromon.entidades.Potrodex;
+/**
+ *
+ * @author Thinkpad
+ */
+public class Potromon {
+
+   public static void main(String[] args) {
+        Entrenador.getAll();}
+}
